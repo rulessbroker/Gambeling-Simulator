a488de3f4cc16c2ee9d3cb6929379dc302ab97c4
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-27 23:28:35 +0530

    Start with stake 00

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,5 +1,15 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+	public static void main(String[] args) {
+		for (int day = 1; day <= 20; day++) {
+			int dayStake = 100;
+			doBet(1);
+		}
+	}
 
+	static int doBet(int bid) {
+
+		return 0;
+	}
 }
