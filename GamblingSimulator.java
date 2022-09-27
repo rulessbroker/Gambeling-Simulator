fb45dd7fc49187eab8bf61fa613a2858c9bbd037
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-27 23:38:34 +0530

    Each month want to know Win or Loss

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,31 +1,49 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+	
 	public static void main(String[] args) {
+		
 		int totalWinningAmount = 0;
+		int winDaysCount = 0;
+		int looseDaysCount = 0;
+		
 		for (int day = 1; day <= 20; day++) {
 			int dayStake = 100;
 			int winningAmount = 0;
 			System.out.println("#day" + day);
+			
 			while (dayStake < 150 && dayStake > 50) {
 				dayStake += doBet(1);
 			}
 			
-			winningAmount += (dayStake - 100);
-			totalWinningAmount += winningAmount;
+			winningAmount = (dayStake - 100);
 			System.out.println("winning amount = " + winningAmount);
-			totalWinningAmount += dayStake;
+			
+			if (winningAmount > 0) {
+				totalWinningAmount = totalWinningAmount + winningAmount;
+				winDaysCount++;
+			} else {
+				looseDaysCount++;
+				totalWinningAmount = totalWinningAmount + winningAmount;
+			}
+			
+			System.out.println("total winning amount " + totalWinningAmount);
 		}
 		
 		System.out.println("\ntotal winning amount " + totalWinningAmount);
+		System.out.println("won days = " + winDaysCount);
+		System.out.println("loose days = " + looseDaysCount);
 	}
 
 	static int doBet(int bid) {
+		
 		int random = (int) (Math.random() * 10) % 2;
 		if (random == 1) {
 			return 1;
 		} else {
 			return -1;
+			
 		}
 	}
 }
