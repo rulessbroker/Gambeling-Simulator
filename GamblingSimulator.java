75de1a054e579969178b0f0dfdc4af84533e1c3c
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-27 23:42:10 +0530

    IF WON contineu playing or stop gambling

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,25 +1,44 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class GamblingSimulator {
-	
+
+	static int totalWinningAmount = 0;
+	static int winDaysCount = 0;
+	static int looseDaysCount = 0;
+	static final int MAX_DAYS = 20;
+	static final int EVERY_DAY_STAKE = 100;
+
 	public static void main(String[] args) {
-		
-		int totalWinningAmount = 0;
-		int winDaysCount = 0;
-		int looseDaysCount = 0;
-		
-		for (int day = 1; day <= 20; day++) {
-			int dayStake = 100;
-			int winningAmount = 0;
-			System.out.println("#day" + day);
+
+		Gambler();
+		if (totalWinningAmount > 0) {
+			System.out.println("you Won ! ");
+			System.out.print("continue playing (y/n) : ");
+			Scanner scanner = new Scanner(System.in);
 			
+			if (scanner.next().equalsIgnoreCase("y")) {
+				Gambler();
+			}
+		} else if (totalWinningAmount < 0) {
+			System.out.println("you lost the game");
+		} else {
+			System.out.println("no win or loss");
+		}
+	}
+
+	static void Gambler() {
+
+		for (int day = 1; day <= MAX_DAYS; day++) {
+			int dayStake = EVERY_DAY_STAKE;
+			int winningAmount;
+			System.out.println("#day" + day);
 			while (dayStake < 150 && dayStake > 50) {
 				dayStake += doBet(1);
 			}
-			
 			winningAmount = (dayStake - 100);
 			System.out.println("winning amount = " + winningAmount);
-			
 			if (winningAmount > 0) {
 				totalWinningAmount = totalWinningAmount + winningAmount;
 				winDaysCount++;
@@ -27,23 +46,21 @@ public class GamblingSimulator {
 				looseDaysCount++;
 				totalWinningAmount = totalWinningAmount + winningAmount;
 			}
-			
-			System.out.println("total winning amount " + totalWinningAmount);
 		}
-		
+
 		System.out.println("\ntotal winning amount " + totalWinningAmount);
 		System.out.println("won days = " + winDaysCount);
 		System.out.println("loose days = " + looseDaysCount);
+
 	}
 
-	static int doBet(int bid) {
+	static int doBet(int bidPrice) {
 		
 		int random = (int) (Math.random() * 10) % 2;
 		if (random == 1) {
-			return 1;
+			return +bidPrice;
 		} else {
-			return -1;
-			
+			return -bidPrice;
 		}
 	}
 }
